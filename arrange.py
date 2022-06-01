c71508f83d96f95b2513080931ace8e02265b23c
ThorMM <76879980+bitdecay1@users.noreply.github.com>
2022-06-01 10:21:34 -0500

    Update arrange.py

--- arrange.py
+++ arrange.py
@@ -1,6 +1,6 @@
 import re
-
-string = "auditor, backup, bin, daemon, games, gnats, irc, libuuid, list, lp, mail, man, mysql, news, nobody, proxy, root, sshd, sync, sys, sysadmin, syslog, uucp, www-data"
+#print a vertical list into a line-by-line list
+string = "gnats, irc, libuuid, list, lp, mail, www-data"
 #The \s matches any whitespace character, and we just replace it with an empty string ''
 string = re.sub(r'\s', '', string).split(',')
 
